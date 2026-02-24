45ba5c54b983a34569e4d3bdfba446982b6474df
Ikerionwu Ifeanyi <44778724+sudo-talon@users.noreply.github.com>
2026-02-24 18:58:36 +0100

    Add user and group settings to start.sh

--- start.sh
+++ start.sh
@@ -6,6 +6,8 @@ cat > /nix/store/jxpzay1xrcphcd05wb7i7vysy89pkmn8-php-8.1.31/etc/php-fpm.conf <<
 daemonize = no
 
 [www]
+user = nobody
+group = nobody
 listen = 127.0.0.1:9000
 pm = dynamic
 pm.max_children = 5
